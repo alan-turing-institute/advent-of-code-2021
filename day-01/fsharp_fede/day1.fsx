6396fca0e87f746400dc4929cd4607787a6b5ac5
fedenanni <nanni.federico@gmail.com>
2021-12-04 10:03:13 +0000

    Still on my F# journey, redone D1 properly.Now D2!

--- day-01/fsharp_fede/day1.fsx
+++ day-01/fsharp_fede/day1.fsx
@@ -1,30 +1,18 @@
 ﻿open System.IO
 
 let lines = File.ReadAllLines("input.txt")       
-let intList = Seq.toList lines 
-            |> Seq.map int
+let intList = Seq.toArray lines 
+            |> Array.map int
 
-let seqWindows = Seq.windowed 2 intList
+let seqWindows = Array.windowed 2 intList
+let countOne = seqWindows |> Array.map (fun x -> if x[1]-x[0]>0 then 1 else 0) |> Array.sum
 
-// ok, i have tried 200 times but i haven't found the proper way  
-// of doing this using sumBy or well, that single line operation instead of this for loop
+printfn "%A" countOne
 
-let mutable x = 0
+let threeGroup = Array.windowed 3 intList
+let seqSum = Array.map Array.sum threeGroup
+let pairs = Array.windowed 2 seqSum
 
-for item in seqWindows do
-    let i = if item[1] - item[0]>0 then 1 else 0
-    x <- x + i
+let countTwo = pairs |> Array.map (fun x -> if x[1]-x[0]>0 then 1 else 0) |> Array.sum
 
-printfn "%i" x
-
-let threeGroup = Seq.windowed 3 intList
-let seqSum = Seq.map Array.sum threeGroup
-let pairs = Seq.windowed 2 seqSum
-
-// once again I am here and i haven't figured out how to do this last step properly
-let mutable y = 0
-for item in pairs do
-    let i = if item[1] - item[0]>0 then 1 else 0
-    y <- y + i
-
-printfn "%i" y
+printfn "%i" countTwo
