f2d12de38f3d6262e9f7160e9c94d6c7384f1ecf
Oscar Giles <ogiles@turing.ac.uk>
2021-12-05 09:13:29 +0000

    format

--- day-05/rust_ogiles/src/main.rs
+++ day-05/rust_ogiles/src/main.rs
@@ -1,8 +1,7 @@
-use std::{ops::Sub, collections::HashMap, cmp};
+use lazy_static::lazy_static;
 use regex::Regex;
 use std::fs;
-use lazy_static::lazy_static;
-
+use std::{cmp, collections::HashMap, ops::Sub};
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 struct Point(i32, i32);
@@ -18,22 +17,19 @@ impl Sub for Point {
     }
 }
 
-
 #[derive(Debug, Copy, Clone, PartialEq)]
 struct Line {
     start: Point,
-    end: Point
+    end: Point,
 }
 
 impl Line {
-
     fn new(start: Point, end: Point) -> Self {
-        Self{start, end}
+        Self { start, end }
     }
 
     /// Parse a &str of the form "0,9 -> 5,9" into a Line
     fn from_str(input: &str) -> Self {
-
         // Avoid recompilation of Regex, which is slow.
         // lazy_static evaluates an expression once and then uses cached value.
         lazy_static! {
@@ -42,26 +38,34 @@ impl Line {
 
         let cap = RE.captures(input).unwrap();
 
-        Self::new(Point(cap[1].parse().unwrap(), cap[2].parse().unwrap()), Point(cap[3].parse().unwrap(), cap[4].parse().unwrap()))
-
+        Self::new(
+            Point(cap[1].parse().unwrap(), cap[2].parse().unwrap()),
+            Point(cap[3].parse().unwrap(), cap[4].parse().unwrap()),
+        )
     }
-    
-    /// Get all the points on a horizontal or vertical line. 
+
+    /// Get all the points on a horizontal or vertical line.
     /// If diagonal line return None
     fn points_on_line(&self, diagonal: bool) -> Option<Vec<Point>> {
-
         match self.end - self.start {
             Point(0, _) => {
-
                 let start = cmp::min(self.start.1, self.end.1);
                 let end = cmp::max(self.start.1, self.end.1);
-                return Some((start..=end).map(|y| Point(self.start.0, y)).collect::<Vec<Point>>());
-            },
+                return Some(
+                    (start..=end)
+                        .map(|y| Point(self.start.0, y))
+                        .collect::<Vec<Point>>(),
+                );
+            }
             Point(_, 0) => {
                 let start = cmp::min(self.start.0, self.end.0);
                 let end = cmp::max(self.start.0, self.end.0);
-                return Some((start..=end).map(|x| Point(x, self.start.1,)).collect::<Vec<Point>>());
-            }, 
+                return Some(
+                    (start..=end)
+                        .map(|x| Point(x, self.start.1))
+                        .collect::<Vec<Point>>(),
+                );
+            }
             _ => {
                 //Implement diagonal line
                 None
@@ -71,10 +75,8 @@ impl Line {
 }
 
 fn part1(input: &Vec<Line>) -> usize {
-
     let mut counter: HashMap<Point, i32> = HashMap::new();
     input.iter().for_each(|line| {
-
         let points = line.points_on_line(false);
 
         if let Some(point) = points {
@@ -86,9 +88,7 @@ fn part1(input: &Vec<Line>) -> usize {
     });
 
     counter.values().filter(|&val| val > &1).count() as usize
-
 }
-
 
 fn main() {
     println!("Hello, world!");
@@ -96,11 +96,9 @@ fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
 
     let all_line: Vec<Line> = input.lines().map(|line| Line::from_str(line)).collect();
-    
+
     println!("Part 1 = {}", part1(&all_line));
 }
-
-
 
 #[cfg(test)]
 mod tests {
@@ -119,18 +117,16 @@ mod tests {
 5,5 -> 8,2
 ";
 
-
     #[test]
     fn test_point_on_line() {
-
         let line = Line::new(Point(0, 1), Point(0, 3)).points_on_line(false);
-        assert_eq!(Some(vec![Point(0,1), Point(0,2), Point(0,3)]), line);
+        assert_eq!(Some(vec![Point(0, 1), Point(0, 2), Point(0, 3)]), line);
 
         let line = Line::new(Point(3, 15), Point(5, 15)).points_on_line(false);
-        assert_eq!(Some(vec![Point(3,15), Point(4,15), Point(5,15)]), line);
+        assert_eq!(Some(vec![Point(3, 15), Point(4, 15), Point(5, 15)]), line);
 
         let line = Line::new(Point(5, 15), Point(3, 15)).points_on_line(false);
-        assert_eq!(Some(vec![Point(3,15), Point(4,15), Point(5,15)]), line);
+        assert_eq!(Some(vec![Point(3, 15), Point(4, 15), Point(5, 15)]), line);
 
         let line = Line::new(Point(0, 0), Point(3, 3)).points_on_line(false);
         assert_eq!(None, line);
@@ -138,15 +134,18 @@ mod tests {
 
     #[test]
     fn test_line_parser() {
-        assert_eq!(Line::from_str("0,9 -> 5,9"), Line::new(Point(0, 9), Point(5,9)));
+        assert_eq!(
+            Line::from_str("0,9 -> 5,9"),
+            Line::new(Point(0, 9), Point(5, 9))
+        );
     }
 
     #[test]
     fn test_part_1s() {
-        let input: Vec<Line> = EXAMPLE_INPUT.lines().map(|line| Line::from_str(line)).collect();
+        let input: Vec<Line> = EXAMPLE_INPUT
+            .lines()
+            .map(|line| Line::from_str(line))
+            .collect();
         assert_eq!(part1(&input), 5);
     }
-
-
-
 }
