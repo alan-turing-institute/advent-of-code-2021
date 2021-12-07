4a3e232cf2ddd116c7d9c49e37e342ec9bc87b9e
Themistoklis Botsas <tbotsas@turing.ac.uk>
2021-12-07 07:20:11 +0000

    Julia day 7

---
+++ day-07/julia_themisbo/day7_part1.jl
@@ -0,0 +1,15 @@
+orig_input = readlines("day7.txt")
+Init_state = hcat(split.(orig_input,",")...) .|> x -> parse(Int64,x)
+
+# positions = sort(unique(Init_state))
+un = unique(Init_state)
+positions = sort(collect(minimum(un):maximum(un)))
+all_dist = zeros(Int64, size(positions,1), 2)
+
+all_dist[:,1] = positions
+
+for (ind, pos) in enumerate(positions)
+    all_dist[ind,2] = sum(abs.(Init_state .- all_dist[ind,1]))
+end
+
+minimum(all_dist[:,2])
---
+++ day-07/julia_themisbo/day7_part2.jl
@@ -0,0 +1,14 @@
+orig_input = readlines("day7.txt")
+Init_state = hcat(split.(orig_input,",")...) .|> x -> parse(Int64,x)
+
+un = unique(Init_state)
+positions = sort(collect(minimum(un):maximum(un)))
+all_dist = zeros(Int64, size(positions,1), 2)
+
+all_dist[:,1] = positions
+
+for (ind, pos) in enumerate(positions)
+    all_dist[ind,2] = sum(binomial.((abs.(Init_state .- all_dist[ind,1])).+1,2))
+end
+
+minimum(all_dist[:,2])
