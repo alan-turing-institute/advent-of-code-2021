87e8e12cd08eecb1193a9f30b4d126a6281286b7
rchan26 <rchan@turing.ac.uk>
2021-12-07 09:08:09 +0000

    change variable name

--- day-07/r_rchan26/day_seven.R
+++ day-07/r_rchan26/day_seven.R
@@ -9,15 +9,15 @@ compute_minimum_cost <- function(initial_state, constant_cost) {
   potential_positions <- min(initial_state):max(initial_state)
   if (constant_cost) {
     # PART ONE: constant cost for movement
-    differences <- sapply(potential_positions, function(pos) sum(abs(initial_state-pos)))
+    cost <- sapply(potential_positions, function(pos) sum(abs(initial_state-pos)))
   } else {
     # PART TWO: increasing cost for movement
-    differences <- sapply(potential_positions, function(pos) {
+    cost <- sapply(potential_positions, function(pos) {
       sum(sapply(abs(initial_state-pos), function(diff) sum(1:diff)))})
   }
-  minimum_cost_position <- potential_positions[which(differences==min(differences))]
+  minimum_cost_position <- potential_positions[which(cost==min(cost))]
   return(list('position' = minimum_cost_position,
-              'fuel_spend' = min(differences)))
+              'fuel_spend' = min(cost)))
 }
 
 # test input
