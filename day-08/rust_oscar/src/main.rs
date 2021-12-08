c4d7d95c3f79156482f6b2434ef0a2bf169cff81
Oscar Giles <ogiles@turing.ac.uk>
2021-12-08 18:56:28 +0000

    An ugly brute

--- day-08/rust_oscar/src/main.rs
+++ day-08/rust_oscar/src/main.rs
@@ -1,6 +1,11 @@
+use itertools::Itertools;
 use lazy_static::lazy_static;
 use regex::Regex;
-use std::fs;
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
+use rayon::prelude::*;
 
 fn part1(input: &str) -> usize {
     lazy_static! {
@@ -13,9 +18,74 @@ fn part1(input: &str) -> usize {
         .sum()
 }
 
+/// Map a signal to a vector of hash sets
+fn signal_to_set(signal: &str, map: &HashMap<char, char>) -> Vec<HashSet<char>> {
+    signal
+        .split_whitespace()
+        .map(|sig| HashSet::from_iter(sig.chars().map(|c| *map.get(&c).unwrap())))
+        .collect()
+}
+
+fn part2(input: &str) -> usize {
+    let numbers: Vec<HashSet<char>> = vec![
+        HashSet::from(['a', 'b', 'c', 'e', 'f', 'g']),
+        HashSet::from(['c', 'f']),
+        HashSet::from(['a', 'c', 'd', 'e', 'g']),
+        HashSet::from(['a', 'c', 'd', 'f', 'g']),
+        HashSet::from(['b', 'c', 'd', 'f']),
+        HashSet::from(['a', 'b', 'd', 'f', 'g']),
+        HashSet::from(['a', 'b', 'd', 'e', 'f', 'g']),
+        HashSet::from(['a', 'c', 'f']),
+        HashSet::from(['a', 'b', 'c', 'd', 'e', 'f', 'g']),
+        HashSet::from(['a', 'b', 'c', 'd', 'f', 'g']),
+    ];
+
+    let keys = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+
+    // Create an iterator over all possible wire to segment mappings
+    input.par_lines().map(|line| line.split_once("|").unwrap()).map(
+        |(original_signal, outputs)| {    
+
+
+            let values = keys.iter().permutations(7).map(|v| {
+                let mut map = HashMap::new();
+                keys.into_iter().zip(v).for_each(|(k, &v)| {
+                    map.insert(k, v);
+                });
+                map
+            });
+
+
+            let find_output = |signal: &str| {
+                let mut check = values.filter(|map| {
+                    signal_to_set(signal, map)
+                        .iter()
+                        .all(|sig| numbers.contains(sig))
+                });
+        
+                let wire_to_segment = check.next().unwrap();
+        
+                signal_to_set(outputs, &wire_to_segment)
+                    .iter()
+                    .map(|v| numbers.iter().position(|n| n == v).unwrap())
+                    .rev()
+                    .enumerate()
+                    .fold(0, |acc, x| {
+                        acc + 10usize.pow(x.0 as u32) * x.1 
+                    })
+                   
+            };
+
+            find_output(original_signal)
+
+        }
+    ).sum()
+}
+
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
     println!("Part 1 = {}", part1(&input));
+    println!("Part 2 = {}", part2(&input));
 }
 
 #[cfg(test)]
