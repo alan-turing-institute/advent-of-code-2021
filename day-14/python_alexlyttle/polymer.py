6bc16aff55d4fb0980ab9869f62448a7f2369fb9
alexlyttle <a.j.lyttle@outlook.com>
2021-12-15 12:20:29 +0000

    Remove test input

--- day-14/python_alexlyttle/polymer.py
+++ day-14/python_alexlyttle/polymer.py
@@ -1,24 +1,5 @@
 import numpy as np
 
-TEST_INPUT = '''NNCB
-
-CH -> B
-HH -> N
-CB -> H
-NH -> C
-HB -> C
-HC -> B
-HN -> C
-NN -> C
-BH -> H
-NC -> B
-NB -> B
-BN -> B
-BB -> N
-BC -> B
-CC -> N
-CN -> C'''
-
 def load_input(file_name):
     with open(file_name, 'r') as file:
         s = file.read()
