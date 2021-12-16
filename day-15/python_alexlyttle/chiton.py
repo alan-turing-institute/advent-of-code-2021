a2f76eabf55da4f5688b42ee19c7df977559b342
alexlyttle <a.j.lyttle@outlook.com>
2021-12-16 10:46:48 +0000

    Update with feature to print minimum risk path

--- day-15/python_alexlyttle/chiton.py
+++ day-15/python_alexlyttle/chiton.py
@@ -32,6 +32,15 @@ def find_adjacency(shape):
         adjacency.append(adj)
     return adjacency
 
+def backpropagate(prev):
+    """Backpropagate path given previous positions."""
+    path = []
+    k = len(prev) - 1  # Start at end
+    while k is not None:
+        path.append(k)
+        k = prev[k]
+    return path[::-1]  # To give list in order
+
 def minimise_risk(risk_map, return_path=False):
     """Simple implementation of Dijkstra's algorithm to minimise risk in map.
     
@@ -41,32 +50,34 @@ def minimise_risk(risk_map, return_path=False):
     Returns:
         int: Total risk for minimum path from top-left to bottom-right of
             risk_map.
-        list of int: List of visited positions in risk_map.
+        list of int: List of minimum path positions in risk_map.
     """
     adj = find_adjacency(risk_map.shape)
     risk = risk_map.ravel()
 
     heap = []  # To keep track of cumulative risk
-    # visited = []  # To keep track of visited positions (e.g. for testing)
-    cum_risk = [-1 for _ in risk]  # Cumulative risk is initially -1 everywhere
+    prev = [None] * len(risk)  # To keep track of previous positions
+    cum_risk = [None] * len(risk)  # Cumulative risk is initially -1 everywhere
 
-    def update(value, position):
+    def update(value, position, prev_position):
         """Update heap, visited positions and cumulative risk"""
         heapq.heappush(heap, (value, position))  # Push risk value and position
-        # visited.append(position)
+        prev[position] = prev_position
         cum_risk[position] = value  # Update cumulative risk at position
 
-    update(0, 0)  # Start at position 0 with a risk value of 0
+    update(0, 0, None)  # Start at position 0 with a risk value of 0
 
     while heap:
         # Pop smallest item on the heap (corresponds to smallest cumulative risk)
         r, i = heapq.heappop(heap)
         for j in adj[i]:
             new_risk = r + risk[j]
-            if cum_risk[j] > -1 and cum_risk[j] <= new_risk:
-                # Ignore if previously visited with a lower cumulative risk
-                continue
-            update(new_risk, j)
+            if cum_risk[j] is None or new_risk < cum_risk[j]:
+                # Only update if not visited or new risk is lower (better)
+                update(new_risk, j, i)
+    
+    if return_path:
+        return cum_risk[-1], backpropagate(prev)
     
     return cum_risk[-1]
 
@@ -95,10 +106,23 @@ if __name__ == '__main__':
                         help='input file name')
     parser.add_argument('-s', '--scale-factor', type=int, default=1,
                         help='scale factor by which to expand the risk map')
+    parser.add_argument('-p', '--print', action='store_true',
+                        help='print minimum risk path')
 
     args = parser.parse_args()
     s = load_input(args.input_file)
     risk_map = expand_riskmap(riskmap_from_str(s), scale_factor=args.scale_factor)
-    min_risk = minimise_risk(risk_map)
-        
+    
+    if args.print:
+        min_risk, path = minimise_risk(risk_map, return_path=True)
+        m = risk_map.astype(str)
+        coords = np.divmod(path, risk_map.shape[1])
+        # Make path bold
+        m[coords] = np.char.add(np.char.add('\033[1m', m[coords]), '\033[0m')
+        m = '\n'.join([''.join(col) for col in m.T])
+        print(m)
+        print('-'*risk_map.shape[1])
+    else:
+        min_risk = minimise_risk(risk_map)
+
     print(f'Min risk = {min_risk}')
