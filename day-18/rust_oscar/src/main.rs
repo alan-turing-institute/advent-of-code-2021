110f053a5b94b2cab6cc73cea9d463326c8bda6d
Oscar Giles <ogiles@turing.ac.uk>
2021-12-29 09:26:18 +0000

    Data structure is hard to work with

--- day-18/rust_oscar/src/main.rs
+++ day-18/rust_oscar/src/main.rs
@@ -4,15 +4,51 @@ fn main() {
     println!("Hello, world!");
 }
 
+struct IterTree<'a> {
+    value: Vec<&'a Num>,
+    stack: Vec<&'a Num>,
+}
+
+impl<'a> IterTree<'a> {
+    fn new(value: Vec<&'a Num>) -> Self {
+        let stack = vec![value[0]];
+        Self { value, stack }
+    }
+}
+
+impl<'a> Iterator for IterTree<'a> {
+    type Item = &'a Num;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.stack.len() > 0 {
+            let node = self.stack.pop().expect("Still values on stack");
+            match node {
+                Num::Regular(_) => {}
+                Num::Pair(tree_node) => {
+                    self.stack.push(&self.value[tree_node.right?]);
+                    self.stack.push(&self.value[tree_node.left?]);
+                }
+            };
+            return Some(node);
+        }
+        None
+    }
+}
+
 #[derive(Debug)]
 struct Tree {
     value: Vec<Num>,
+}
+
+enum Side {
+    Left,
+    Right,
 }
 
 impl Tree {
     fn new(tree: Vec<Num>) -> Self {
         Self { value: tree }
     }
+
     fn get_num(&self, idx: usize) -> &Num {
         &self.value[idx]
     }
@@ -22,30 +58,9 @@ impl Tree {
     fn left_num(&self, idx: usize) -> &Num {
         self.get_num(self.get_num(idx).unwrap_snailtree().left.unwrap())
     }
-    fn walk_tree(&self) -> Option<usize> {
-        // Find nodes at depth 3 (nested in 4 brackets)
-        // Explode the left node
-        let mut stack = vec![&self.value[0]];
-
-        while stack.len() > 0 {
-            let node = stack.pop()?;
-            match node {
-                Num::Regular(literal) => {
-                    println!("val = {}", literal);
-                }
-                Num::Pair(tree_node) => {
-                    if tree_node.depth == 3 {
-                        // return Some(tree_node.left?);
-                    }
-                    stack.push(&self.value[tree_node.right?]);
-                    stack.push(&self.value[tree_node.left?]);
-                    
-                }
-            };
-        }
-        None
+    fn iter(&self) -> IterTree {
+        IterTree::new(self.value.iter().collect())
     }
-
     fn first_right_reg_idx(&self, num: &Num) -> Option<usize> {
         let mut parent = num.unwrap_snailtree().parent;
 
@@ -75,9 +90,27 @@ impl Tree {
 
         None
     }
+    fn is_left_of_parent(&self, num: &Num) -> bool {
+        match num.parent_idx() {
+            Some(parent) if self.left_num(parent) == num => true,
+            _ => false,
+        }
+    }
+
+    fn is_right_of_parent(&self, num: &Num) -> bool {
+        match num.parent_idx() {
+            Some(parent) if self.right_num(parent) == num => true,
+            _ => false,
+        }
+    }
 
     fn explode(&mut self, explode_idx: usize) {
+
         let value_to_explode = self.get_num(explode_idx);
+        let value_to_explode_parent = value_to_explode.parent_idx();
+
+        let is_left = self.is_left_of_parent(value_to_explode);
+        let is_right =self.is_right_of_parent(value_to_explode);
 
         if let &Num::Pair(p) = value_to_explode {
             let left = self.get_num(p.left.unwrap()).unwrap_regular();
@@ -86,16 +119,38 @@ impl Tree {
             let first_right = self.first_right_reg_idx(value_to_explode);
             let first_left = self.first_left_reg_idx(value_to_explode);
 
+            
             if let Some(r) = first_right {
-                self.value[r] = Num::Regular(self.value[r].unwrap_regular() + right)
+                let old_reg = self.value[r].unwrap_regular();
+                self.value[r] = Num::Regular(Regular::new(old_reg.parent, old_reg.value + right.value))
+                
+            }
+            if let Some(l) = first_left {
+                let old_reg = self.value[l].unwrap_regular();
+                self.value[l] = Num::Regular(Regular::new(old_reg.parent, old_reg.value + left.value))
             }
 
-            if let Some(l) = first_left {
-                self.value[l] = Num::Regular(self.value[l].unwrap_regular() + left)
-            }
-            self.value[explode_idx] = Num::Regular(0);
+            self.value[explode_idx] = Num::Regular(Regular::new(value_to_explode_parent, 0));
+
+            // Remove left and right values
             self.value.remove(p.left.unwrap());
             self.value.remove(p.right.unwrap());
+
+            
+            if is_left {
+                let value = &mut self.value[value_to_explode_parent.unwrap()];
+                value.unwrap_snailtree().left = None;
+            }
+            if is_right {
+                let value = &mut self.value[value_to_explode_parent.unwrap()];
+                value.unwrap_snailtree().right = None;
+            }
+            
+            
+
+            // p.left = None;
+            // p.right = None;
+
         }
     }
 }
@@ -105,19 +160,34 @@ impl FromStr for Tree {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut value: Vec<Num> = vec![];
         let mut stack: Vec<usize> = vec![];
+        let mut depth: u8 = 0;
 
         for i in 0..s.len() {
+
             let c = &s[i..i + 1];
+
+            let tree_size = value.len();
+            let last_pair_idx = stack
+                .iter()
+                .filter(| &x| matches!(value[*x], Num::Pair(_)))
+                .map(|x| *x)
+                .rev()
+                .nth(0);
+
+            // let last_pair_idx = if stack.is_empty() {None} else {Some(stack.len() - 1)};
+            let last_pair = if stack.is_empty() {
+                None
+            } else {
+                Some(&mut value[last_pair_idx.unwrap()])
+            };
 
             match c {
                 "[" => {
-                    let p = Num::Pair(SnailTree::new(None, None, None, 0));
-
-                    let tree_size = value.len();
+                    depth += 1;
+                    let p = Num::Pair(SnailTree::new(last_pair_idx, None, None, depth - 1));
 
                     if tree_size > 0 {
-                        let last_pair_idx = stack.len() - 1;
-                        let last_pair = &mut value[stack[last_pair_idx]];
+                        let last_pair = last_pair.unwrap();
 
                         if last_pair.unwrap_snailtree().left.is_none() {
                             if let Num::Pair(v) = last_pair {
@@ -135,6 +205,7 @@ impl FromStr for Tree {
                 }
                 "," => {}
                 "]" => {
+                    depth -= 1;
                     stack.pop();
                 }
                 _ => {
@@ -142,9 +213,8 @@ impl FromStr for Tree {
                     if longer_c.contains(",") | longer_c.contains("]") {
                         longer_c = c;
                     }
-                    let last_pair_idx = stack.len() - 1;
-                    let tree_size = value.len();
-                    let last_pair = &mut value[stack[last_pair_idx]];
+
+                    let last_pair = last_pair.unwrap();
 
                     if last_pair.unwrap_snailtree().left.is_none() {
                         if let Num::Pair(v) = last_pair {
@@ -156,18 +226,19 @@ impl FromStr for Tree {
                         }
                     }
 
-                    value.push(Num::Regular(longer_c.parse::<u8>().unwrap()));
+                    value.push(Num::Regular(Regular::new(
+                        last_pair_idx,
+                        longer_c.parse::<u8>().unwrap(),
+                    )));
                 }
             }
         }
-
-        // for c in s.chars() {
 
         Ok(Tree { value: value })
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 struct SnailTree {
     parent: Option<usize>,
     left: Option<usize>,
@@ -186,14 +257,26 @@ impl SnailTree {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct Regular {
+    parent: Option<usize>,
+    value: u8,
+}
+
+impl Regular {
+    fn new(parent: Option<usize>, value: u8) -> Self {
+        Self { parent, value }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
 enum Num {
-    Regular(u8),
+    Regular(Regular),
     Pair(SnailTree),
 }
 
 impl Num {
-    fn unwrap_regular(&self) -> u8 {
+    fn unwrap_regular(&self) -> Regular {
         match self {
             Num::Regular(reg) => *reg,
             _ => panic!("Not regular"),
@@ -204,6 +287,12 @@ impl Num {
         match self {
             Num::Regular(_) => panic!("Not a SnailTree"),
             Num::Pair(pair) => *pair,
+        }
+    }
+    fn parent_idx(&self) -> Option<usize> {
+        match self {
+            Num::Regular(reg) => reg.parent,
+            Num::Pair(pair) => pair.parent,
         }
     }
 }
@@ -224,42 +313,96 @@ mod tests {
 
     use super::*;
 
-    #[test]
-    fn test() {
-        // [[[[[9,8],1],2],3],4]
+    // #[test]
+    // fn test() {
+    //     // [[[[[9,8],1],2],3],4]
 
-        let t = vec![
-            Num::Pair(SnailTree::new(None, Some(2), Some(1), 0)),
-            Num::Regular(4),
-            Num::Pair(SnailTree::new(Some(0), Some(4), Some(3), 1)),
-            Num::Regular(3),
-            Num::Pair(SnailTree::new(Some(2), Some(6), Some(5), 2)),
-            Num::Regular(2),
-            Num::Pair(SnailTree::new(Some(4), Some(8), Some(7), 3)),
-            Num::Regular(1),
-            Num::Pair(SnailTree::new(Some(6), Some(9), Some(10), 4)),
-            Num::Regular(9),
-            Num::Regular(8),
-        ];
+    //     let t = vec![
+    //         Num::Pair(SnailTree::new(None, Some(2), Some(1), 0)),
+    //         Num::Regular(4),
+    //         Num::Pair(SnailTree::new(Some(0), Some(4), Some(3), 1)),
+    //         Num::Regular(3),
+    //         Num::Pair(SnailTree::new(Some(2), Some(6), Some(5), 2)),
+    //         Num::Regular(2),
+    //         Num::Pair(SnailTree::new(Some(4), Some(8), Some(7), 3)),
+    //         Num::Regular(1),
+    //         Num::Pair(SnailTree::new(Some(6), Some(9), Some(10), 4)),
+    //         Num::Regular(9),
+    //         Num::Regular(8),
+    //     ];
 
-        let mut tree = Tree::new(t);
+    //     let mut tree = Tree::new(t);
 
-        println!("{:?}", tree.value);
+    //     println!("{:?}", tree.value);
 
-        tree.walk_tree();
-        // tree.explode(tree.walk_tree().unwrap());
+    //     // tree.walk_tree();
+    //     // tree.explode(tree.walk_tree().unwrap());
 
-        // println!("{:?}", tree.value);
-    }
+    //     // println!("{:?}", tree.value);
+    // }
 
     #[test]
     fn test_parse() {
         let s = "[[[[[9,8],1],2],3],4]";
+        // let s = "[1,2]";
+        // let s = "[[1,2],3]";
+        // let s = "[9,[8,7]]";
 
-        let tree = Tree::from_str(s).unwrap();
+        let mut tree = Tree::from_str(s).unwrap();
 
-        println!("{:?}", tree.value);
+        println!("{:#?}", tree.value);
 
-        tree.walk_tree();
+        let explode_pair = tree.iter().filter(|&x| matches!(x, Num::Pair(_))).filter(|&x| x.unwrap_snailtree().depth == 4).nth(0).unwrap();
+
+        println!("explode = {:?}", explode_pair);
+
+        let explode_idx = if tree.is_left_of_parent(explode_pair) {
+            tree.value[explode_pair.parent_idx().unwrap()].unwrap_snailtree().left.unwrap()
+        } else {
+            tree.value[explode_pair.parent_idx().unwrap()].unwrap_snailtree().right.unwrap()
+        };
+
+
+        tree.explode(explode_idx);
+
+        println!("\n\n{:#?}", tree.value);
+
+        // tree.walk_tree();
+
+        // let walked = tree
+        //     .iter()
+        //     .filter(|&x| matches!(x, Num::Regular(_)))
+        //     .collect::<Vec<_>>();\
+
+        // println!("{:?}", tree.iter().collect::<Vec<_>>());
+
+        let out = tree.iter().fold(String::new(), |mut acc, x| {
+
+            // println!("{:?}", x);
+
+            if tree.is_right_of_parent(x) {
+                acc.push(',');
+            }
+
+            match x {
+                Num::Regular(reg) => {
+                    acc.push_str(&format!("{}", reg.value));
+
+                },
+                Num::Pair(_) => {acc.push('[');}
+            };
+
+            // if tree.is_left_of_parent(x) {
+            //     acc.push(',');
+            // }
+            if tree.is_right_of_parent(x) {
+                acc.push(']');
+            }
+
+            acc
+        });
+        println!("\n{:?}", out);
+
+        // assert_eq!(out, s);
     }
 }
